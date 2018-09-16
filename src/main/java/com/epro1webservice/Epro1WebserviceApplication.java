db229a859a4452b83e250e4c3da0550845c1d09f
Miguel Henriquez <elderriqui@gmail.com>
2018-09-15 19:09:43 -0600

    first commit

---
+++ src/main/java/com/epro1webservice/Epro1WebserviceApplication.java
@@ -0,0 +1,12 @@
+package com.epro1webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Epro1WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Epro1WebserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epro1webservice/Epro1WebserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epro1webservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Epro1WebserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
